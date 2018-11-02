49d13dd3fb957bd77098940eaf75407096ccc94f
jhanjhenyi <jhanjhenyi@gmail.com>
2018-11-02 12:41:04 +0800

    Update fetch more news and read button get news detail modal

--- nba_news/news/serializers.py
+++ nba_news/news/serializers.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'image_url', 'source_url')
+        fields = ('id', 'title', 'content', 'image_url', 'publish_date', 'source_url')
