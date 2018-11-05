f1a615f7ed9e00915b89926d6c74ca88f7202fb8
jhanjhenyi <jhanjhenyi@gmail.com>
2018-11-05 17:42:14 +0800

    Update ALLOWED_HOSTS

--- nba_news/nba_news/settings.py
+++ nba_news/nba_news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!skm3ibne)^9q_cvdkwrghlq%zg_ph!^2ms-r@_p!rdkg96)wm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
