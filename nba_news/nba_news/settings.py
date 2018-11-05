631024b90055e86e327ae81ea2a2c6a159d984d7
jhanjhenyi <jhanjhenyi@gmail.com>
2018-11-05 17:50:53 +0800

    Update DATABASES settings to environ val

--- nba_news/nba_news/settings.py
+++ nba_news/nba_news/settings.py
@@ -77,21 +77,14 @@ WSGI_APPLICATION = 'nba_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoproject',
-        'USER': 'djangoproject',
-        'HOST': 'localhost',
-        'PASSWORD': '0000',
-        'PORT': '',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
